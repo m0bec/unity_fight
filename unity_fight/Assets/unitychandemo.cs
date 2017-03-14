3c7f659734d76d50844f742f64106a236bee9afb
mobec <mobec622@outlook.jp>
2017-03-14 20:53:33 +0900

    バックから左に移動できるように編集

--- unity_fight/Assets/unitychandemo.cs
+++ unity_fight/Assets/unitychandemo.cs
@@ -17,7 +17,7 @@ public class unitychandemo : MonoBehaviour {
 	void Update () {
         animator_state = animator.GetCurrentAnimatorStateInfo(0);
         
-        if (Input.GetKey("up") || Input.GetKey("right") || Input.GetKey("left"))
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.DownArrow))
         {
             animator.SetBool("is_running", true);
         }
@@ -51,18 +51,42 @@ public class unitychandemo : MonoBehaviour {
                 {
                     transform.Rotate(0, 5, 0);
                     mem_rotate += 5.0f;
-                }
-            }
-
-            if (Input.GetKey("left"))
-            {
-                if (mem_rotate > -90.0f)
+                }else if(mem_rotate > 90.0f)
                 {
                     transform.Rotate(0, -5, 0);
                     mem_rotate -= 5.0f;
                 }
             }
 
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                if (mem_rotate > -90.0f)
+                {
+                    transform.Rotate(0, -5, 0);
+                    mem_rotate -= 5.0f;
+                }else if(mem_rotate < -90.0f)
+                {
+                    transform.Rotate(0, 5, 0);
+                    mem_rotate += 5.0f;
+                }
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                if(Mathf.Abs(mem_rotate) < 180.0f)
+                {
+                    if(mem_rotate > 0.0f)
+                    {
+                        transform.Rotate(0, 5, 0);
+                        mem_rotate += 5.0f;
+                    }
+                    else
+                    {
+                        transform.Rotate(0, -5, 0);
+                        mem_rotate -= 5.0f;
+                    }
+                }
+            }
             transform.position += transform.forward * 0.05f;
         }   
     }
