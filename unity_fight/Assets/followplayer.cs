3a591e271831717aa135a16d235e4b131b1a1e5a
mobec <mobec622@outlook.jp>
2017-03-14 18:37:55 +0900

    カメラがunity-chanに追従するようになった

--- unity_fight/Assets/followplayer.cs
+++ unity_fight/Assets/followplayer.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class followplayer : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+    public Transform target;
+    private Vector3 offset;
+    // Use this for initialization
+    void Start () {
+        offset = GetComponent<Transform>().position - target.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        GetComponent<Transform>().position = target.position + offset;
 	}
 }
