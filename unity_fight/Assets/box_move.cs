04badd89e15320f82be5a8820d780041ecd6118c
mobec <mobec622@outlook.jp>
2017-03-14 16:10:34 +0900

    一定時間でbox-unityがジャンプする

--- unity_fight/Assets/box_move.cs
+++ unity_fight/Assets/box_move.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class box_move : MonoBehaviour {
+    public Rigidbody rigidbody;
+    public float measure_time;
 
-	// Use this for initialization
-	void Start () {
-		
+    private float force;
+    private float jump_time;
+    // Use this for initialization
+    void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        force = 5.0f;
+        jump_time = 5.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        measure_time += Time.deltaTime;
+
+        if(measure_time > jump_time)
+        {
+            rigidbody.AddForce(0, 0, force, ForceMode.Impulse);
+            measure_time = 0.0f;
+        }
+    }
 }
