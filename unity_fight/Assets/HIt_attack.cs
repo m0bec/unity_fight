089452e1cfd9fb16da220df24badca23dc3dcb18
mobec <mobec622@outlook.jp>
2017-03-16 10:56:09 +0900

    unityboxが武器との接触で飛ぶようになった

--- unity_fight/Assets/HIt_attack.cs
+++ unity_fight/Assets/HIt_attack.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HIt_attack : MonoBehaviour {
-
+    new public Rigidbody rigidbody;
+    private box_move Box_move;
+    private float impulse;
     // Use this for initialization
     void Start () {
-		
+        rigidbody = GetComponent<Rigidbody>();
+        impulse = 1000.0f;
 	}
 	
 	// Update is called once per frame
@@ -16,9 +19,10 @@ public class HIt_attack : MonoBehaviour {
 
     void OnCollisionEnter(Collision collision)
     {
+        Box_move = GetComponent<box_move>();
         if (collision.gameObject.tag == "weapon")
         {
-
+            rigidbody.AddForce(-Box_move.vec_x * impulse, 0, -Box_move.vec_z * impulse, ForceMode.Impulse);
         }
     }
 }
