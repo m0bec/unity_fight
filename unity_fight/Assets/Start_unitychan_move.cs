bd66d6a3f65052a64addb7bee20a5a429ebf8d7f
mobec <mobec622@outlook.jp>
2017-03-20 23:14:34 +0900

    boxが出現ー＞驚きー＞ターンして逃げる

--- unity_fight/Assets/Start_unitychan_move.cs
+++ unity_fight/Assets/Start_unitychan_move.cs
@@ -6,9 +6,16 @@ public class Start_unitychan_move : MonoBehaviour {
     private Vector3 unity_vec;
     private GameObject[] unity_box;
     private Animator animator;
+
+    private float escape_time;
+    private float measure_time;
+
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
+
+        escape_time = 1.0f;
+        measure_time = 0.0f;
 	}
 	
 	// Update is called once per frame
@@ -36,7 +43,26 @@ public class Start_unitychan_move : MonoBehaviour {
         }
         else
         {
-            animator.SetBool("Sup_flag", true);
+            measure_time += Time.deltaTime;
+
+            if (measure_time < escape_time)
+            {
+                animator.SetBool("Sup_flag", true);
+            }
+            else
+            {
+                animator.SetBool("Sup_flag", false);
+            }
+
+            if(measure_time > escape_time)
+            {
+                animator.SetBool("Run_flag", true);
+                this.transform.position += new Vector3(-0.05f, 0.0f, 0.0f);
+                if (Mathf.DeltaAngle(270.0f, transform.eulerAngles.y) > 1.0f)
+                {
+                    transform.Rotate(0.0f, -5.0f, 0.0f);
+                }
+            }
         }
 	}
 }
