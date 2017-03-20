a90d0f81702444807f4e1587e44cee681df2028c
mobec <mobec622@outlook.jp>
2017-03-20 22:34:00 +0900

    unity-chanが画面にはいってからこっち向くまでのアニメ―ション

---
+++ unity_fight/Assets/Start_unitychan_move.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_unitychan_move : MonoBehaviour {
+    private Vector3 unity_vec;
+    private GameObject[] unity_box;
+    private Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        unity_vec = this.transform.position;
+        unity_box = GameObject.FindGameObjectsWithTag("box_unity");
+        if (unity_box.Length == 0)
+        {
+            if (unity_vec.x < -1.0f)
+            {
+                animator.SetBool("Run_flag", true);
+                this.transform.position += new Vector3(0.05f, 0.0f, 0.0f);
+            }
+            else
+            {
+                if (Mathf.DeltaAngle(0.0f, transform.eulerAngles.y) > 1.0f)
+                {
+                    transform.Rotate(0.0f, -5.0f, 0.0f);
+                }
+                else
+                {
+                    animator.SetBool("Run_flag", false);
+                }
+            }
+        }
+	}
+}
