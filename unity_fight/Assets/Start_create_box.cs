f4d322e17740cd55eae4d02210bba373222836e5
mobec <mobec622@outlook.jp>
2017-03-21 12:17:22 +0900

    unitychanがワープして戻ってくる

--- unity_fight/Assets/Start_create_box.cs
+++ unity_fight/Assets/Start_create_box.cs
@@ -7,9 +7,11 @@ public class Start_create_box : MonoBehaviour {
     public float create_time;
     private float measure_time;
 
+    public bool create_flag;
     public GameObject boxunity;
 	// Use this for initialization
 	void Start () {
+        create_flag = true;
         vec = this.transform.position;
         create_time = 5.0f;
         measure_time = 0.0f;
@@ -17,12 +19,15 @@ public class Start_create_box : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        measure_time += Time.deltaTime;
-
-        if(measure_time > create_time)
+        if (create_flag)
         {
-            Instantiate(boxunity, vec, Quaternion.Euler(0, -90, 0));
-            measure_time = 0.0f;
+            measure_time += Time.deltaTime;
+
+            if (measure_time > create_time)
+            {
+                Instantiate(boxunity, vec, Quaternion.Euler(0, -90, 0));
+                create_flag = false;
+            }
         }
 	}
 }
--- unity_fight/Assets/Start_unitychan_move.cs
+++ unity_fight/Assets/Start_unitychan_move.cs
@@ -8,66 +8,94 @@ public class Start_unitychan_move : MonoBehaviour {
     private GameObject[] unity_box;
     private Animator animator;
 
+    private bool animate_flag;
+
     private float move_speed;
     private float escape_time;
     private float measure_time;
     private float move_distance;
+
+    private float idle_poition;
+    private float return_distance;
+    private bool return_flag;
+
+    private float step;
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
         unity_start_vec = this.transform.position;
+        animate_flag = true;
+
         escape_time = 1.0f;
         measure_time = 0.0f;
         move_speed = 0.01f;
         move_distance = 0.43f;
+        return_flag = false;
+
+        step = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         unity_vec = this.transform.position;
         unity_box = GameObject.FindGameObjectsWithTag("box_unity");
-        if (unity_box.Length == 0)
+
+        if (animate_flag)
         {
-            if (Mathf.Abs(unity_start_vec.x - unity_vec.x) < move_distance)
+            if (unity_box.Length == 0)
             {
-                animator.SetBool("Run_flag", true);
-                this.transform.position += new Vector3(move_speed, 0.0f, 0.0f);
-            }
-            else
-            {
-                if (Mathf.DeltaAngle(0.0f, transform.eulerAngles.y) > 1.0f)
+                if (Mathf.Abs(unity_start_vec.x - unity_vec.x) < move_distance && step == 0.0f)
                 {
-                    transform.Rotate(0.0f, -5.0f, 0.0f);
+                    animator.SetBool("Run_flag", true);
+                    this.transform.position += new Vector3(move_speed, 0.0f, 0.0f);
                 }
                 else
                 {
-                    animator.SetBool("Run_flag", false);
+                    step = 1.0f;
+                    idle_poition = this.transform.position.x;
+                    if (Mathf.DeltaAngle(0.0f, transform.eulerAngles.y) > 1.0f)
+                    {
+                        transform.Rotate(0.0f, -5.0f, 0.0f);
+                    }
+                    else
+                    {
+                        animator.SetBool("Run_flag", false);
+                    }
                 }
-            }
-        }
-        else
-        {
-            measure_time += Time.deltaTime;
-
-            if (measure_time < escape_time)
-            {
-                animator.SetBool("Sup_flag", true);
             }
             else
             {
-                animator.SetBool("Sup_flag", false);
-            }
+                measure_time += Time.deltaTime;
 
-            if(measure_time > escape_time)
-            {
-                animator.SetBool("Run_flag", true);
-                animator.SetBool("Esp_flag", true);
-                this.transform.position += new Vector3(-move_speed, 0.0f, 0.0f);
-                if (Mathf.DeltaAngle(270.0f, transform.eulerAngles.y) > 1.0f)
+                if (measure_time < escape_time && step == 1.0f)
                 {
-                    transform.Rotate(0.0f, -5.0f, 0.0f);
+                    animator.SetBool("Sup_flag", true);
+                }
+                else
+                {
+                    animator.SetBool("Sup_flag", false);
+                    step = 2.0f;
+                }
+
+                if (measure_time > escape_time && step == 2.0f)
+                {
+                    animator.SetBool("Run_flag", true);
+                    animator.SetBool("Esp_flag", true);
+                    this.transform.position += new Vector3(-move_speed, 0.0f, 0.0f);
+                    if (Mathf.DeltaAngle(270.0f, transform.eulerAngles.y) > 1.0f)
+                    {
+                        transform.Rotate(0.0f, -5.0f, 0.0f);
+                    }
+                }
+
+                if(Mathf.Abs(idle_poition - this.transform.position.x) > move_distance && !return_flag && step == 2.0f)
+                {
+                    this.transform.position = new Vector3(-2.0f, 0.4f, -1.49f);
+                    return_flag = true;
+
                 }
             }
         }
+        
 	}
 }
