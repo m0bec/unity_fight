ea71811539df3fbc5a075e35c4d736bfdecbf775
mobec <mobec622@outlook.jp>
2017-03-26 15:06:48 +0900

    カメラがキャラを中心に回転する

--- unity_fight/Assets/followplayer.cs
+++ unity_fight/Assets/followplayer.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class followplayer : MonoBehaviour {
-    public Transform target;
-    private Vector3 offset;
+    private GameObject gameobject;
+    private Vector3 before_position;
+    private float rotate_speed;
     // Use this for initialization
     void Start () {
-        offset = GetComponent<Transform>().position - target.position;
-	}
+        rotate_speed = 50.0f;
+
+        gameobject = GameObject.Find("unitychan");
+        before_position = gameobject.transform.position;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        GetComponent<Transform>().position = target.position + offset;
-	}
+        transform.position += gameobject.transform.position - before_position;
+        before_position = gameobject.transform.position;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.RotateAround(gameobject.transform.position, Vector3.up, rotate_speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.RotateAround(gameobject.transform.position, Vector3.up, -rotate_speed * Time.deltaTime);
+        }
+        
+
+    }
 }
