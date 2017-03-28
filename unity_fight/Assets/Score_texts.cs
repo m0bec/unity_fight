4b0b6f4f720c94ca0ab2bf2d4185b69130c9a797
mobec <mobec622@outlook.jp>
2017-03-28 16:17:24 +0900

    gameover時文字を表示

--- unity_fight/Assets/Score_texts.cs
+++ unity_fight/Assets/Score_texts.cs
@@ -6,16 +6,31 @@ using UnityEngine.UI;
 public class Score_texts : MonoBehaviour
 {
     public int score;
+    private GameObject game_system;
+    private Game_system.state_info state_info_; 
     // Use this for initialization
     void Start()
     {
+        game_system = GameObject.Find("Gamesystem");
         score = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.GetComponent<Text>().text = "Score " + score.ToString() + "point";
+        state_info_ = game_system.GetComponent<Game_system>().state;
+        if (state_info_ == Game_system.state_info.Gameover)
+        {
+            this.GetComponent<Text>().text = "GAME OVER";
+        }
+        else if (state_info_ == Game_system.state_info.Playing)
+        {
+            this.GetComponent<Text>().text = "Score " + score.ToString() + "point";
+        }
+        else if (state_info_  == Game_system.state_info.Pouse)
+        {
+
+        }
     }
 }
 
