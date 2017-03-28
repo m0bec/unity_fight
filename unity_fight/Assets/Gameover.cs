7d1c51f56dfb03232f0c15d6ec06be23ddd31be2
mobec <mobec622@outlook.jp>
2017-03-28 16:02:23 +0900

    gameover時にアクション

--- unity_fight/Assets/Gameover.cs
+++ unity_fight/Assets/Gameover.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gameover : MonoBehaviour {
-    Game_system gamesystem;
+    private Game_system gamesystem;
+    private Animator animator;
+    private AnimatorStateInfo animator_state;
     public bool gameover_flag;
 	// Use this for initialization
 	void Start () {
         gameover_flag = false;
-	}
+        animator = GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (gameover_flag)
+        {
+            animator_state = animator.GetCurrentAnimatorStateInfo(0);
+            if(animator_state.fullPathHash == Animator.StringToHash("Base Layer.Gameover"))
+            {
+                animator.SetBool("is_gameover", false);
+            }
+        }
 	}
 
     private void OnCollisionEnter(Collision collision)
@@ -20,6 +30,7 @@ public class Gameover : MonoBehaviour {
         if(collision.gameObject.tag == "box_unity")
         {
             gameover_flag = true;
+            animator.SetBool("is_gameover", true);
         }
     }
 }
