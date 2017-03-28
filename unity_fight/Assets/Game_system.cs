e7d5e9121fb47eb4a5bcbf310371aa7472ada60f
mobec <mobec622@outlook.jp>
2017-03-28 17:07:06 +0900

    gameoverからタイトルに移動

--- unity_fight/Assets/Game_system.cs
+++ unity_fight/Assets/Game_system.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Game_system : MonoBehaviour {
     public enum state_info
@@ -10,10 +11,17 @@ public class Game_system : MonoBehaviour {
 
     public state_info state;
     GameObject unitychan;
+
+    float time;
+    float gameover_time;
+    bool gameover_next_flag;
 	// Use this for initialization
 	void Start () {
         unitychan = GameObject.Find("unitychan");
         state = state_info.Playing;
+        time = 0.0f;
+        gameover_time = 2.0f;
+        gameover_next_flag = false;
 	}
 	
 	// Update is called once per frame
@@ -21,6 +29,22 @@ public class Game_system : MonoBehaviour {
         if (unitychan.GetComponent<Gameover>().gameover_flag)
         {
             state = state_info.Gameover;
+        }
+
+        if(state == state_info.Gameover)
+        {
+            time += Time.deltaTime;
+            if(time > gameover_time)
+            {
+                gameover_next_flag = true;
+            }
+            if (gameover_next_flag)
+            {
+                if(Input.GetKey(KeyCode.Z))
+                {
+                    SceneManager.LoadScene("start");
+                }
+            }
         }
 	}
 }
