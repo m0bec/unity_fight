1f70c332e7899fcab0a77c69a4f7efa3d7335b21
mobec <mobec622@outlook.jp>
2017-03-28 12:47:30 +0900

    スムーズに方向転換できるようになった

--- unity_fight/Assets/unitychandemo.cs
+++ unity_fight/Assets/unitychandemo.cs
@@ -14,7 +14,7 @@ public class unitychandemo : MonoBehaviour {
     void Start () {
         animator = GetComponent<Animator>();
         camera = GameObject.Find("Main Camera");
-        rotate_speed = 10.0f;
+        rotate_speed = 30.0f;
         margine = 5.0f;
 	}
 	
@@ -36,46 +36,62 @@ public class unitychandemo : MonoBehaviour {
         {
             if (Input.GetKey(KeyCode.UpArrow))
             {
-                if(Mathf.DeltaAngle(camera.transform.eulerAngles.y, transform.eulerAngles.y) < -(margine))
+                if(Mathf.DeltaAngle(camera.transform.eulerAngles.y, transform.eulerAngles.y) < -(rotate_speed))
                 {
                     transform.Rotate(0, rotate_speed, 0);
-                }else if(Mathf.DeltaAngle(camera.transform.eulerAngles.y, transform.eulerAngles.y) > margine)
+                }else if(Mathf.DeltaAngle(camera.transform.eulerAngles.y, transform.eulerAngles.y) > rotate_speed)
                 {
                     transform.Rotate(0, -(rotate_speed), 0);
+                }
+                else
+                {
+                    transform.rotation = Quaternion.Euler(0.0f, camera.transform.eulerAngles.y, 0.0f);
                 }
             }
                     
             if (Input.GetKey(KeyCode.RightArrow))
             {
-                if (Mathf.DeltaAngle(camera.transform.eulerAngles.y + 90.0f, transform.eulerAngles.y) < -(margine))
+                if (Mathf.DeltaAngle(camera.transform.eulerAngles.y + 90.0f, transform.eulerAngles.y) < -(rotate_speed))
                 {
                     transform.Rotate(0, rotate_speed, 0);
-                }else if(Mathf.DeltaAngle(camera.transform.eulerAngles.y + 90.0f, transform.eulerAngles.y) > margine)
+                }else if(Mathf.DeltaAngle(camera.transform.eulerAngles.y + 90.0f, transform.eulerAngles.y) > rotate_speed)
                 {
                     transform.Rotate(0, -(rotate_speed), 0);
+                }
+                else
+                {
+                    transform.rotation = Quaternion.Euler(0.0f, camera.transform.eulerAngles.y + 90.0f, 0.0f);
                 }
             }
 
             if (Input.GetKey(KeyCode.LeftArrow))
             {
-                if (Mathf.DeltaAngle(camera.transform.eulerAngles.y + 270.0f, transform.eulerAngles.y) < -(margine))
+                if (Mathf.DeltaAngle(camera.transform.eulerAngles.y + 270.0f, transform.eulerAngles.y) < -(rotate_speed))
                 {
                     transform.Rotate(0, rotate_speed, 0);
-                }else if(Mathf.DeltaAngle(camera.transform.eulerAngles.y + 270.0f, transform.eulerAngles.y) > margine)
+                }else if(Mathf.DeltaAngle(camera.transform.eulerAngles.y + 270.0f, transform.eulerAngles.y) > rotate_speed)
                 {
                     transform.Rotate(0, -(rotate_speed), 0);
+                }
+                else
+                {
+                    transform.rotation = Quaternion.Euler(0.0f, camera.transform.eulerAngles.y + 270.0f, 0.0f);
                 }
             }
 
             if (Input.GetKey(KeyCode.DownArrow))
             {
-                if (Mathf.DeltaAngle(camera.transform.eulerAngles.y + 180.0f, transform.eulerAngles.y) < -(margine))
+                if (Mathf.DeltaAngle(camera.transform.eulerAngles.y + 180.0f, transform.eulerAngles.y) < -(rotate_speed))
                 {
                     transform.Rotate(0, rotate_speed, 0);
                 }
-                else if (Mathf.DeltaAngle(camera.transform.eulerAngles.y + 180.0f, transform.eulerAngles.y) > margine)
+                else if (Mathf.DeltaAngle(camera.transform.eulerAngles.y + 180.0f, transform.eulerAngles.y) > rotate_speed)
                 {
                     transform.Rotate(0, -(rotate_speed), 0);
+                }
+                else
+                {
+                    transform.rotation = Quaternion.Euler(0.0f, camera.transform.eulerAngles.y + 180.0f, 0.0f);
                 }
             }
             transform.position += transform.forward * 0.05f;
