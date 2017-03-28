5b6da3e344af4ec0e9b76591843c2b5ba6ed2182
mobec <mobec622@outlook.jp>
2017-03-28 21:55:09 +0900

    gameover時のメッセージ

--- unity_fight/Assets/Score_texts.cs
+++ unity_fight/Assets/Score_texts.cs
@@ -7,12 +7,20 @@ public class Score_texts : MonoBehaviour
 {
     public int score;
     private GameObject game_system;
-    private Game_system.state_info state_info_; 
+    private Game_system.state_info state_info_;
+
+    private GameObject gameover1;
+    private GameObject gameover2;
+    private GameObject gameover3;
     // Use this for initialization
     void Start()
     {
         game_system = GameObject.Find("Gamesystem");
+
         score = 0;
+        gameover1 = GameObject.Find("Gameover1");
+        gameover2 = GameObject.Find("Gameover2");
+        gameover3 = GameObject.Find("Gameover3");
     }
 
     // Update is called once per frame
@@ -21,7 +29,11 @@ public class Score_texts : MonoBehaviour
         state_info_ = game_system.GetComponent<Game_system>().state;
         if (state_info_ == Game_system.state_info.Gameover)
         {
-            this.GetComponent<Text>().text = "GAME OVER\n" + "Score " + score.ToString();
+            this.GetComponent<Text>().text = "";
+            gameover1.GetComponent<Text>().text = "GAME OVER";
+            gameover2.GetComponent<Text>().text = "Score " + score.ToString();
+            gameover3.GetComponent<Text>().text = "Please push Z key";
+            //this.GetComponent<Text>().text = "GAME OVER\n" + "Score " + score.ToString() + "\nPlease push Z key";
         }
         else if (state_info_ == Game_system.state_info.Playing)
         {
