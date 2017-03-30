0b556152f2a8e27aeb7ec8d297a78cba723aeeda
mobec <mobec622@outlook.jp>
2017-03-31 00:22:34 +0900

    アルゴリズムを高速化
    FindをStartでするようにした

--- unity_fight/Assets/box_move.cs
+++ unity_fight/Assets/box_move.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class box_move : MonoBehaviour {
     private GameObject game_system;
+    GameObject unity_chan;
     private Game_system.state_info state_info_;
 
     new public Rigidbody rigidbody;
@@ -21,7 +22,7 @@ public class box_move : MonoBehaviour {
     // Use this for initialization
     void Start () {
         game_system = GameObject.Find("Gamesystem");
-
+        unity_chan = GameObject.Find("unitychan");
         rigidbody = GetComponent<Rigidbody>();
         force = 5.0f;
         jump_time = 1.85f;
@@ -34,15 +35,15 @@ public class box_move : MonoBehaviour {
         if (Time.timeScale > 0)
         {
             measure_time += Time.deltaTime;
-            vector = GameObject.Find("unitychan").transform.position;
+            vector = unity_chan.transform.position;
 
             vec_x = vector.x - this.transform.position.x;
             vec_z = vector.z - this.transform.position.z;
             //回転
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(vector.x, 0, vector.z)), rotation_speed);
             vec_direct = Mathf.Sqrt(Mathf.Pow(vec_x, 2.0f) + Mathf.Pow(vec_z, 2.0f));
-            vec_x = (vec_x / vec_direct) * vec_scale;
-            vec_z = (vec_z / vec_direct) * vec_scale;
+            vec_x = (vec_x / vec_direct) * vec_scale * Time.deltaTime;
+            vec_z = (vec_z / vec_direct) * vec_scale * Time.deltaTime;
             this.transform.position += new Vector3(vec_x, 0, vec_z);
             this.transform.Rotate(0, rotation, 0);
 
